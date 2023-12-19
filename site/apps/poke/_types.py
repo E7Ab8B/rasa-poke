01daf63e90ad8c109814509fdc6d213b54e8c2db
E7Ab8B <140745017+E7Ab8B@users.noreply.github.com>
2023-12-20 00:41:44 +0100

    refactor(poke): Move types to a seperate module

---
+++ site/apps/poke/_types.py
@@ -0,0 +1,98 @@
+from __future__ import annotations
+
+from typing import TypedDict
+
+
+class Pokemon(TypedDict):
+    """Basic Pokemon information that exists in the Pokemon list endpoint."""
+
+    name: str
+    url: str
+
+
+class PokemonList(TypedDict):
+    """List of basic Pokemon information."""
+
+    count: int
+    next: str
+    previous: str
+    results: list[Pokemon]
+
+
+class Stat(TypedDict):
+    """Stat information of a Pokemon."""
+
+    name: int
+    url: int
+
+
+class StatInfo(TypedDict):
+    """Detailed information about a Pokemon's stat."""
+
+    base_stat: int
+    effort: int
+    stat: Stat
+
+
+class Type(TypedDict):
+    """Type information of a Pokemon."""
+
+    name: str
+    url: str
+
+
+class TypeInfo(TypedDict):
+    """Detailed information about a Pokemon's type."""
+
+    slot: int
+    type: Type
+
+
+class PokemonInfo(TypedDict):
+    """Detailed Pokemon information which is returned on a detail endpoint."""
+
+    id: int
+    name: str
+    order: int
+    sprites: dict
+    stats: list[StatInfo]
+    types: list[TypeInfo]
+
+
+class EffectEntry(TypedDict):
+    """Entry describing the effect of an item or ability."""
+
+    effect: str
+    short_effect: str
+
+
+class Item(TypedDict):
+    """Item information."""
+
+    name: str
+    url: str
+
+
+class ItemInfo(TypedDict):
+    """Detailed information about an item."""
+
+    id: int
+    name: str
+    url: str
+    effect_entries: list[EffectEntry]
+    sprites: dict[str, str]
+
+
+class BerryInfo(TypedDict):
+    """Information about a berry."""
+
+    id: int
+    name: str
+    item: Type
+
+
+class BerryItemInfo(TypedDict):
+    """Detailed information about a berry."""
+
+    berry: BerryInfo
+    item: ItemInfo
--- site/apps/poke/utils/requests.py
+++ site/apps/poke/utils/requests.py
@@ -1,112 +1,25 @@
 from __future__ import annotations
 
 import asyncio
-from typing import TYPE_CHECKING, TypedDict
+from typing import TYPE_CHECKING
 
 import aiohttp
 
 if TYPE_CHECKING:
     from collections.abc import Sequence
 
+    from apps.poke._types import (
+        BerryInfo,
+        BerryItemInfo,
+        ItemInfo,
+        PokemonInfo,
+        PokemonList,
+    )
+
 POKEMON_API = 'https://pokeapi.co/api/v2/'
 POKEMON_ENDPOINT = f'{POKEMON_API}pokemon/'
 ITEM_ENDPOINT = f'{POKEMON_API}item/'
 BERRY_ENDPOINT = f'{POKEMON_API}berry/'
-
-
-class Pokemon(TypedDict):
-    """Basic Pokemon information that exists in the Pokemon list endpoint."""
-
-    name: str
-    url: str
-
-
-class PokemonList(TypedDict):
-    """List of basic Pokemon information."""
-
-    count: int
-    next: str
-    previous: str
-    results: list[Pokemon]
-
-
-class Stat(TypedDict):
-    """Stat information of a Pokemon."""
-
-    name: int
-    url: int
-
-
-class StatInfo(TypedDict):
-    """Detailed information about a Pokemon's stat."""
-
-    base_stat: int
-    effort: int
-    stat: Stat
-
-
-class Type(TypedDict):
-    """Type information of a Pokemon."""
-
-    name: str
-    url: str
-
-
-class TypeInfo(TypedDict):
-    """Detailed information about a Pokemon's type."""
-
-    slot: int
-    type: Type
-
-
-class PokemonInfo(TypedDict):
-    """Detailed Pokemon information which is returned on a detail endpoint."""
-
-    id: int
-    name: str
-    order: int
-    sprites: dict
-    stats: list[StatInfo]
-    types: list[TypeInfo]
-
-
-class EffectEntry(TypedDict):
-    """Entry describing the effect of an item or ability."""
-
-    effect: str
-    short_effect: str
-
-
-class Item(TypedDict):
-    """Item information."""
-
-    name: str
-    url: str
-
-
-class ItemInfo(TypedDict):
-    """Detailed information about an item."""
-
-    id: int
-    name: str
-    url: str
-    effect_entries: list[EffectEntry]
-    sprites: dict[str, str]
-
-
-class BerryInfo(TypedDict):
-    """Information about a berry."""
-
-    id: int
-    name: str
-    item: Type
-
-
-class BerryItemInfo(TypedDict):
-    """Detailed information about a berry."""
-
-    berry: BerryInfo
-    item: ItemInfo
 
 
 async def retrieve_pokemon_list(limit: int = -1, offset: int = 0) -> PokemonList:
